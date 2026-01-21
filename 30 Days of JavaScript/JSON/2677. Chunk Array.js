d7a497725579c2887d7089ebdc18aa4c44d186c1
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-01-21 16:40:43 +0200

    Added daily 30 Days of JS problem solutions

---
+++ 30 Days of JavaScript/JSON/2677. Chunk Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const returnArr = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        returnArr.push(arr.slice(i, i + size));
+    }
+
+    return returnArr;
+};
