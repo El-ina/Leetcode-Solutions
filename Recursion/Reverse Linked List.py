147e5986833246869e91551a05f6a640158178d4
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-02-15 08:35:25 +0200

    Reverse Linked List Solution Added

---
+++ Recursion/Reverse Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if head is None or head.next is None:
+            return head
+        
+        t = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return t
